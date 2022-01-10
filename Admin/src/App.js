e1e9d68984ec4f7627981b38ad9f7366b737bd27
Chamano Ramashia <chamano.sr@gmail.com>
2022-01-10 15:59:58 +0200

    debuggin

--- Admin/src/App.js
+++ Admin/src/App.js
@@ -4,9 +4,8 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  Redirect,
 } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
@@ -18,43 +17,27 @@ import Login from "./pages/login/Login";
 import { useSelector } from "react-redux";
 
 function App() {
-  const admin = useSelector((state) => state.user.currentUser.isAdmin);
+  const admin = useSelector((state) => state.user.currentUser?.isAdmin);
   return (
     <Router>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
+      <Routes>
+        <Route path="/login" element={<Login />} />
         {admin && (
           <>
             <Topbar />
             <div className="container">
               <Sidebar />
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/users">
-                <UserList />
-              </Route>
-              <Route path="/user/:userId">
-                <User />
-              </Route>
-              <Route path="/newUser">
-                <NewUser />
-              </Route>
-              <Route path="/products">
-                <ProductList />
-              </Route>
-              <Route path="/product/:productId">
-                <Product />
-              </Route>
-              <Route path="/newproduct">
-                <NewProduct />
-              </Route>
+              <Route exact path="/" element={<Home/>} />
+              <Route path="/users" element={<UserList/>} />
+              <Route path="/user/:userId" element={<User/>} />
+              <Route path="/newUser" element={<NewUser/>} />
+              <Route path="/products" element={<ProductList/>} />
+              <Route path="/product/:productId" element={<Product/>} />
+              <Route path="/newproduct" element={<NewProduct/>} />
             </div>
           </>
         )}
-      </Switch>
+      </Routes>
     </Router>
   );
 }
